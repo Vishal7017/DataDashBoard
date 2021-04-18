3a644f2fe3ab5d667899960e657292261f902f1e
Vishal7017 <wavhalev@gmail.com>
2021-04-18 19:30:10 +0530

    Mann

---
+++ worldbankapp/routes.py
@@ -0,0 +1,16 @@
+from worldbankapp import app
+
+from flask import render_template
+import pandas as pd
+from wrangling_scripts.wrangling import data_wrangling
+
+
+data = data_wrangling()
+
+print(data)
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html', data_set = data)
+     
+
